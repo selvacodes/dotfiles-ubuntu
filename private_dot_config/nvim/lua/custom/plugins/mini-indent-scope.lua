ddcabf0880c114fcd49f39cc48735688032d8209
selva <selva.g@workativ.com>
2025-07-31 23:42:25 +0530

    wip

--- private_dot_config/nvim/lua/custom/plugins/mini-indent-scope.lua
+++ private_dot_config/nvim/lua/custom/plugins/mini-indent-scope.lua
@@ -1,19 +1,21 @@
-return {{
-    "echasnovski/mini.indentscope",
-    version = false,
-    event = "BufEnter",
-    opts = {
-        symbol = "│",
-        options = {
-            try_as_border = true
-        }
-    },
-    init = function()
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = {"help", "Trouble", "trouble", "lazy", "mason", "notify"},
-            callback = function()
-                vim.b.miniindentscope_disable = true
-            end
-        })
-    end
-}}
+return {
+	{
+		"echasnovski/mini.indentscope",
+		version = false,
+		event = "BufEnter",
+		opts = {
+			symbol = "│",
+			options = {
+				try_as_border = true,
+			},
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "help", "Trouble", "trouble", "lazy", "mason", "notify" },
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+		end,
+	},
+}
--- private_dot_config/nvim/lua/custom/plugins/mini-visits.lua
+++ private_dot_config/nvim/lua/custom/plugins/mini-visits.lua
@@ -1,17 +1,11 @@
 local M = {
-  'echasnovski/mini.visits',
-  version = false,
-  config = function()
-    local mini_visits = require 'mini.visits'
-    -- vim.opt.laststatus = 3
-
-    local opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-    mini_visits.setup(opts)
-  end,
+	"echasnovski/mini.visits",
+	version = false,
+	config = function()
+		local mini_visits = require("mini.visits")
+		local opts = {}
+		mini_visits.setup(opts)
+	end,
 }
 
 return M
